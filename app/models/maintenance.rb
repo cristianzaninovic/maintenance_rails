d38f82485e588f4d530f2867db453d5963f6b648
cristianzaninovic <czaninovic@gmail.com>
2023-09-06 04:13:47 -0300

    ERD model diagram pdf generator gem installed
    - check erd.pdf

--- app/models/maintenance.rb
+++ app/models/maintenance.rb
@@ -2,6 +2,7 @@ class Maintenance < ApplicationRecord
   belongs_to :motor
   belongs_to :user
   belongs_to :city
+  has_many :maintenance_material_lists
   has_many :materials, through: :maintenance_material_lists
   enum maintenance_class: [:preventive, :corrective]
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,5 @@
 class Material < ApplicationRecord
+  has_many :maintenance_material_lists
   has_many :maintenances, through: :maintenance_material_lists
   validates :name, presence: true
 end
